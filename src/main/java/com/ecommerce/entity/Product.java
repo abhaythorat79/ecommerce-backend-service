64632d88137565d59b5e59a719ef7a9ae0c4a54e
Abhay Thorat <abhaythorat79@gmail.com>
2025-12-23 23:47:41 +0800

    Initial commit: Spring Boot ecommerce backend setup

---
+++ src/main/java/com/ecommerce/entity/Product.java
@@ -0,0 +1,21 @@
+package com.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private double price;
+    private int stock;
+
+    @ManyToOne
+    private Category category;
+}
---
+++ src/main/java/com/ecommerce/entity/User.java
@@ -0,0 +1,21 @@
+package com.ecommerce.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+}
